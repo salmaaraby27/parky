a04191e8d0c4cb921cbd35e2dbc6dc184d9b30e4
salmaaraby27 <124284337+salmaaraby27@users.noreply.github.com>
2025-06-21 21:32:59 +0300

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('parking.urls')),
+    path('/', include('parking.urls')),
 ]
 
 if settings.DEBUG:
